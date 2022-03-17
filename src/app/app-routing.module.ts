ad58b91cc16a07e040ec95dbb436fcaf4d73d84a
Josebratt <josebratt@gmail.com>
2022-03-17 17:55:19 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './posts/dash-board/dash-board.component';
+import { PostDetailsComponent } from './posts/post-details/post-details.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/blog' },
+  { path: 'dash-board', component: DashBoardComponent },
+  { path: 'blog', component: PostListComponent },
+  { path: 'blog/:id', component: PostDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
