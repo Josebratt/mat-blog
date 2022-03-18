a8ebb963cea0f740c8612eb1a32390bb44ce52b6
Josebratt <josebratt@gmail.com>
2022-03-18 17:25:50 -0400

    Update post-details.component.ts

--- src/app/posts/post-details/post-details.component.ts
+++ src/app/posts/post-details/post-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
 import { Post } from 'src/app/interfaces/post';
 import { PostService } from 'src/app/services/post.service';
 
@@ -12,21 +13,35 @@ export class PostDetailsComponent implements OnInit {
 
   post: Post | undefined;
 
+  // time!: Timestamp<any | undefined>;
+
   constructor( 
     private postService: PostService,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
-    this.getPost();
-    console.log(this);
-    
+    this.getPost();         
   }
 
-  getPost(){
+  getPost() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     return this.postService.getPostDetails(id!).subscribe(
-      data => this.post = data
+      data => {
+        this.post = data; 
+        // this.time = data?.published
+      }
     )
+  }
+
+  update() {
+    const formData = {
+      title: this.post?.title,
+      content: this.post?.content
+    };
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.postService.updatePost(id!, formData)
   }
 
 }
