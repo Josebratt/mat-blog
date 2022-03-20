ae7e90dc68509adbdf10eb4533b37b147bf3ba7a
Josebratt <josebratt@gmail.com>
2022-03-20 17:40:10 -0400

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +13,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
