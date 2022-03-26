10c4ede4f17d90e26bc2680afd26b733df2ac270
Josebratt <josebratt@gmail.com>
2022-03-26 10:51:42 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 import { DashBoardComponent } from './posts/dash-board/dash-board.component';
 import { PostDetailsComponent } from './posts/post-details/post-details.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['blog']);
+
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/blog' },
-  { path: 'dash-board', component: DashBoardComponent },
+  { path: 'dash-board', component: DashBoardComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
   { path: 'blog', component: PostListComponent },
   { path: 'blog/:id', component: PostDetailsComponent },
 ];
